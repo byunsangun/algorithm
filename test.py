74a438cc89853e6b043dd3a8cea94bf7e51a5318
byunsangun <xdcfvgg@naver.com>
2021-01-20 13:41:22 +0900

    그리디 - 큰 수의 법칙

--- test.py
+++ test.py
@@ -1,1 +1,41 @@
-print("Hello world!")
+n, m, k = map(int, input().split())
+arr = list(map(int,input().split()))
+arr.sort()
+
+
+""" 1st way by using loop
+first = arr[n-1]
+second = arr[n-2]
+temp = 0
+sum = 0
+
+for i in range(m):
+    if temp < k:
+        sum = sum + first
+        temp = temp + 1
+    else:
+        temp = 0
+        sum = sum + second
+
+print(sum)
+"""
+
+
+# 2nd way which is better than first one
+first = arr[n-1]
+second = arr[n-2]
+
+total_first = int(m / (k+1)) * k
+total_second = int(m / (k+1))
+remain_first = int(m % (k+1))
+
+
+result = (first * (total_first + remain_first)) + (second * total_second)
+print(result)
+
+
+
+
+
+
+
