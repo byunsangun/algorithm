6ee21561c266313506833341c869b348cc9dd851
byunsangun <xdcfvgg@naver.com>
2021-01-22 23:44:41 +0900

    구현 - 게임 개발

--- test.py
+++ test.py
@@ -1,15 +1,63 @@
-position = input()
+n, m = map(int, input().split())
 
-move_x = [-2, -2, -1, 1, 2, 2, -1, 1]
-move_y = [-1, 1, 2, 2, 1, -1, -2, -2]
+d = [[0] * m for _ in range(n)]
 
-count = 0
+x, y, direction = map(int, input().split())
+d[x][y] = 1
 
-for i in range(8):
-    x = chr(ord(position[0]) - move_x[i])
-    y = int(position[1]) - move_y[i]
+game_map = []
+move = [[-1,0], [0,1], [1, 0], [0, -1]]
+count = 1
+turn_time = 0
 
-    if x >= 'a' and x <= 'h' and y >= 1 and y <= 8:
+for i in range(n):
+    game_map.append(list(map(int, input().split())))
+
+
+def turn_left():
+    global direction
+
+    if direction == 0:
+        direction = 3
+    else:
+        direction -= 1
+
+
+while True:
+
+    turn_left()
+
+    temp_x = x + move[direction][0]
+    temp_y = y + move[direction][1]
+
+    if d[temp_x][temp_y] == 0 and game_map[temp_x][temp_y] == 0:
+
+        x = temp_x
+        y = temp_y
+
+        d[x][y] = 1
         count += 1
+        turn_time = 0
+        print("count : ", count)
+        continue
+
+    else:
+        print("turn!!!")
+        turn_time += 1
+
+    if turn_time == 4:
+        temp_x = x - move[direction][0]
+        temp_y = y - move[direction][1]
+
+        if game_map[temp_x][temp_y] == 0:
+            x = temp_x
+            y = temp_y
+
+        else:
+            break
+
+        turn_time = 0
+
 
 print(count)
+
