12b2a854207dedcc6d506ed9b29352e2445ed8cd
byunsangun <xdcfvgg@naver.com>
2021-01-22 12:55:01 +0900

    구현 - 왕실의 나이트

--- test.py
+++ test.py
@@ -1,40 +1,15 @@
+position = input()
 
-h = int(input())
+move_x = [-2, -2, -1, 1, 2, 2, -1, 1]
+move_y = [-1, 1, 2, 2, 1, -1, -2, -2]
+
 count = 0
 
-for i in range(h+1):
-    for j in range(60):
-        for k in range(60):
-            if '3' in str(i) + str(j) + str(k):
-                count += 1
+for i in range(8):
+    x = chr(ord(position[0]) - move_x[i])
+    y = int(position[1]) - move_y[i]
+
+    if x >= 'a' and x <= 'h' and y >= 1 and y <= 8:
+        count += 1
 
 print(count)
-
-
-"""
-n = int(input())
-result = 0
-
-for hour in range(0, n+1):
-    first_hour = hour // 10
-    second_hour = hour % 10
-    if first_hour == 3 or second_hour == 3:
-        result += 3600
-        print("Time : ", hour, minute, second, " HOUR result : ", result)
-    else:
-        for minute in range(0, 60):
-            first_minute = minute // 10
-            second_minute = minute % 10
-            if first_minute == 3 or second_minute == 3:
-                result += 60
-                print("Time : ", hour, minute, second, " MINUTE result : ", result)
-            else:
-                for second in range(0,60):
-                    first_second = second // 10
-                    second_second = second % 10
-                    if first_second == 3 or second_second == 3:
-                        result += 1
-                        print("Time : ", hour, minute, second, " SECOND result : ", result)
-
-print(result)
-"""
