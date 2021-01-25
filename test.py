17fb06da8029769b9b12d75fec2ab35be7a71da5
byunsangun <xdcfvgg@naver.com>
2021-01-26 00:06:03 +0900

    DFS/BFS - 예제 코드

--- test.py
+++ test.py
@@ -1,63 +1,67 @@
-n, m = map(int, input().split())
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end='')
 
-d = [[0] * m for _ in range(n)]
-
-x, y, direction = map(int, input().split())
-d[x][y] = 1
-
-game_map = []
-move = [[-1,0], [0,1], [1, 0], [0, -1]]
-count = 1
-turn_time = 0
-
-for i in range(n):
-    game_map.append(list(map(int, input().split())))
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 
 
-def turn_left():
-    global direction
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7],
+]
 
-    if direction == 0:
-        direction = 3
-    else:
-        direction -= 1
+visited = [False] * 9
 
-
-while True:
-
-    turn_left()
-
-    temp_x = x + move[direction][0]
-    temp_y = y + move[direction][1]
-
-    if d[temp_x][temp_y] == 0 and game_map[temp_x][temp_y] == 0:
-
-        x = temp_x
-        y = temp_y
-
-        d[x][y] = 1
-        count += 1
-        turn_time = 0
-        print("count : ", count)
-        continue
-
-    else:
-        print("turn!!!")
-        turn_time += 1
-
-    if turn_time == 4:
-        temp_x = x - move[direction][0]
-        temp_y = y - move[direction][1]
-
-        if game_map[temp_x][temp_y] == 0:
-            x = temp_x
-            y = temp_y
-
-        else:
-            break
-
-        turn_time = 0
+# 정의된 DFS 함수 호출
+dfs(graph, 1, visited)
 
 
-print(count)
+
+
+
+from collections import deque
+
+def bfs(graph, start, visited):
+
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7],
+]
+
+visited = [False] * 9
+
+
+# 정의된 BFS 함수 호출
+bfs(graph, 1, visited)
+
+
 
