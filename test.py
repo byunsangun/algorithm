d485161c5f2cd349fa1d166997ee562edd95dc27
byunsangun <xdcfvgg@naver.com>
2021-01-27 01:03:35 +0900

    DFS/BFS - 음료수 얼려 먹기

--- test.py
+++ test.py
@@ -1,67 +1,31 @@
-def dfs(graph, v, visited):
-    visited[v] = True
-    print(v, end='')
+n, m = map(int,input().split())
 
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
+graph = []
+for i in range(n):
+    graph.append(list(map(int, list(input()))))
 
 
-graph = [
-    [],
-    [2, 3, 8],
-    [1, 7],
-    [1, 4, 5],
-    [3, 5],
-    [3, 4],
-    [7],
-    [2, 6, 8],
-    [1, 7],
-]
+def dfs(x,y):
+    if x<= -1 or x >= n or y <= -1 or y >= m:
+        return False
 
-visited = [False] * 9
+    if graph[x][y] == 0:
+        graph[x][y] = 1
 
-# 정의된 DFS 함수 호출
-dfs(graph, 1, visited)
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
+        return True
+
+    return False
 
 
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i,j) == True:
+            result += 1
 
 
-
-from collections import deque
-
-def bfs(graph, start, visited):
-
-    queue = deque([start])
-    visited[start] = True
-
-    while queue:
-        v = queue.popleft()
-        print(v, end=' ')
-
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
-
-
-graph = [
-    [],
-    [2,3,8],
-    [1,7],
-    [1,4,5],
-    [3,5],
-    [3,4],
-    [7],
-    [2,6,8],
-    [1,7],
-]
-
-visited = [False] * 9
-
-
-# 정의된 BFS 함수 호출
-bfs(graph, 1, visited)
-
-
-
+print(result)
