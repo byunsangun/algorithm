361279d2c7702782050d06d2d06e8bba2f732d62
byunsangun <xdcfvgg@naver.com>
2021-01-30 13:50:59 +0900

    정렬 - 위에서 아래로

--- test.py
+++ test.py
@@ -1,37 +1,9 @@
-from collections import deque
+n = int(input())
 
+arr = []
 
-n, m = map(int,input().split())
+for _ in range(n):
+    arr.append(int(input()))
 
-graph = []
-for i in range(n):
-    graph.append(list(map(int, list(input()))))
-
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def bfs(x,y):
-    queue = deque()
-    queue.append((x,y))
-
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-
-            if graph[nx][ny] == 0:
-                continue
-
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-
-    return graph[n-1][m-1]
-
-
-print(bfs(0,0))
+arr = sorted(arr, reverse=True)
+print(arr)
