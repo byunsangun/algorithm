82b7d6b455f2615e692f5c0c9ea9e1fe77bf6fb0
byunsangun <xdcfvgg@naver.com>
2021-02-05 16:14:34 +0900

    다이나믹 프로그래밍 - 개미 전사

--- test.py
+++ test.py
@@ -1,23 +1,20 @@
-x = int(input())
+n = int(input())
+array = list(map(int, input().split()))
 
-d = [0] * 30001
+d = [0] * 101
+d[1] = array[0]
+d[2] = max(array[0], array[1])
 
-for i in range(2, x+1):
-
-    #현재의 수에서 1을 빼는 경우
-    d[i] = d[i - 1] + 1
-
-    # 현재의 수가 2로 나누어 지는 경우
-    if i % 2 == 0:
-        d[i] = min(d[i], d[i//2] + 1)
-
-    # 현재의 수가 3로 나누어 지는 경우
-    if i % 3 == 0:
-        d[i] = min(d[i], d[i//3] + 1)
-
-    # 현재의 수가 5로 나누어 지는 경우
-    if i % 5 == 0:
-        d[i] = min(d[i], d[i//5] + 1)
+for i in range(3, n+1):
+    d[i] = max(d[i-2] + array[i-1] , d[i-1])
 
 
-print(d[x])
+print(d[n])
+
+
+
+
+
+
+
+
