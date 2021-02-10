28fa827bd8bc7f1a1788ba418eb18ef6a5672712
byunsangun <xdcfvgg@naver.com>
2021-02-10 11:49:30 +0900

    다이나믹 프로그래밍 - 효율적인 화폐 구성

--- test.py
+++ test.py
@@ -1,15 +1,22 @@
-n = int(input())
+n, m = map(int, input().split())
 
-d = [0] * 1001
+array = []
+d = [10001] * 10001
+d[0] = 0
 
-d[1] = 1
-d[2] = 3
+for _ in range(n):
+    array.append(int(input()))
 
-for i in range(3, n+1):
-    d[i] = d[i - 2] * 2 + d[i - 1]
+for i in range(1, m+1):
+    for j in array:
+        if i - j >= 0:
+            if d[i - j] != 10001:
+                d[i] = min(d[i], d[i - j] + 1)
 
 
-
-
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
 
 
