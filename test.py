1775cb749f9d9d20f0a098d9eedc938b7e59c74a
byunsangun <xdcfvgg@naver.com>
2021-02-10 16:08:32 +0900

    최단 경로 - 개선된 다익스트라 최단 경로 알고리즘

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import sys
+import heapq
 input = sys.stdin.readline
 INF = int(1e9)
 
@@ -19,41 +20,31 @@ distance = [INF] * (n+1)
 
 for _ in range(m):
     a, b, c = map(int, input().split())
-
-    #a번 노드에서 b번 노드로 가는 비용이 c라는 의미
-    graph[a].append((b,c))
-
-
-#방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
-def get_smallest_node():
-    min_value = INF
-    index = 0
-    for i in range(1, n+1):
-        if distance[i] < min_value and not visited[i]:
-            min_value = distance[i]
-            index = i
-        return index
+    graph[a].append((b,c))       #a번 노드에서 b번 노드로 가는 비용이 c라는 의미
 
 
 def dijstra(start):
-    # 시작 노드에 대해서 초기화
+    q = []
+    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽
+    heapq.heappush(q, (0, start))
     distance[start] = 0
-    visited[start] = True
-    for j in graph[start]:
-        distance[j[0]] = j[1]          # ex) distance[2] = 2, distance[3] = 5
+    while q:
 
-    #시작 노드를 제외한 전체 n - 1 개의 노드에 대한 반복
-    for i in range(n-1):
-        #현재 최단 거리가 가장 짧은 노드를 꺼내서, 방문 처리
-        now = get_smallest_node()
-        visited[now] = True
+        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        dist, now = heapq.heappop(q)
 
-        #현재 노드와 연결된 다른 노드를 확인
-        for j in graph[now]:
-            cost = distance[now] + j[1]
-            #현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
-            if cost < distance[j[0]]:
-                distance[j[0]] = cost
+        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+        if distance[now] < dist:
+            continue
+
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
 
 # 다익스트라 알고리즘 수행
@@ -62,10 +53,8 @@ dijstra(start)
 
 # 모든 노드로 가기 위한 최단 거리를 출력
 for i in range(1, n+1):
-    # 도달할 수 없는 경우, 무한이라고 출력
-    if distance[i] == INF:
+    if distance[i] == INF:        # 도달할 수 없는 경우, 무한이라고 출력
         print("INFINITY")
 
-    # 도달할 수 있는 경우 거리를 출력
-    else:
+    else:                         # 도달할 수 있는 경우 거리를 출력
         print(distance[i])
